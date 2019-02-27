c7001678fe91334220d3f79d816949b99d0a7b7a
Hayato KAJIYAMA <kajiyamah61@COAMAC17000052.local>
2019-02-27 11:13:01 +0900

    add: zooming

---
+++ src/main.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+  new Zooming({
+    bgOpacity: 0.5,
+    scaleBase: 0.6
+  }).listen('.img-zoomable')
+})
+
+console.log("Hi! I'm glad to see you!")
+
+
