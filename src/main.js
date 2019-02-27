69ce2d38e6f2a64f3275cdc2e842d82836f61e33
Hayato KAJIYAMA <kajiyamah61@COAMAC17000052.local>
2019-02-27 13:31:05 +0900

    fix: variaus

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
   new Zooming({
+    enableGrab: true, // ズーム時の画像移動の許可
+    preloadImage: true, // 画像切り替えの許可
+    bgColor: "rgb(0, 0, 0)",
     bgOpacity: 0.5,
-    scaleBase: 0.6
+    scaleBase: 0.8,
+    scaleExtra: 2
   }).listen('.img-zoomable')
 })
 
-console.log("Hi! I'm glad to see you!")
+console.log("Hello! I'm glad to see you!")
 
 
