2e37aed8832932dad392218a7184157917b0e400
hyakt <hyakt0@gmail.com>
2019-07-02 11:27:31 +0900

    fix: js

--- src/main.js
+++ src/main.js
@@ -1,19 +1,21 @@
-const productColor = '#EBCB8B'
-const ossColor = '#B48EAD'
-const researchColor = '#A3BE8C'
-
-document.addEventListener('DOMContentLoaded', function () {
-  new Zooming({
-    enableGrab: true, // ズーム時の画像移動の許可
-    preloadImage: true, // 画像切り替えの許可
-    bgColor: "rgb(0, 0, 0)",
-    bgOpacity: 0.5,
-    scaleBase: 0.8,
-    scaleExtra: 2
-  }).listen('.img-zoomable')
-})
+console.log("Hello! I’m job hunting now! please email me: hyakt0@gmail.com ")
 
 (window.onload = () => {
+  const productColor = '#EBCB8B'
+  const ossColor = '#B48EAD'
+  const researchColor = '#A3BE8C'
+
+  document.addEventListener('DOMContentLoaded', () => {
+    new Zooming({
+      enableGrab: true, // ズーム時の画像移動の許可
+      preloadImage: true, // 画像切り替えの許可
+      bgColor: "rgb(0, 0, 0)",
+      bgOpacity: 0.5,
+      scaleBase: 0.8,
+      scaleExtra: 2
+    }).listen('.img-zoomable')
+  })
+
   Array.prototype.forEach.call(document.getElementsByClassName('tag'), (t) => {
     const tag = t.childNodes[0]
     if (tag.className === 'product') tag.parentElement.parentElement.style.backgroundColor = productColor
@@ -22,6 +24,5 @@ document.addEventListener('DOMContentLoaded', function () {
   })
 })()
 
-console.log("Hello! I'm glad to see you!")
 
 
