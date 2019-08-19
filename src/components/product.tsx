a6135d40622416e79b65265a8af754150ac52264
hyakt <hyakt0@gmail.com>
2019-08-20 01:16:20 +0900

    update: product

--- src/components/product.tsx
+++ src/components/product.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import { css } from "@emotion/core"
-import { useStaticQuery, graphql } from "gatsby"
-import Img from "gatsby-image"
+import { css } from '@emotion/core'
+import { useStaticQuery, graphql } from 'gatsby'
+import Img from 'gatsby-image'
 
 import SectionHeader from './sectionheader'
 import Colors from '../constants/Colors'
@@ -9,13 +9,13 @@ import { rhythm } from '../utils/typography'
 
 type Product = {
   name: string,
-  date: string,
-  description: string,
-  techStack: Array<string>,
+  date?: string,
+  description?: string,
+  techStack?: Array<string>,
   badge?: string,
-  webpage: string,
+  webpage?: string,
   github?: string,
-  image: any
+  image?: any
 }
 
 export default () => {
@@ -23,7 +23,49 @@ export default () => {
     query {
       regist: file(relativePath: { eq: "product/regist.png" }) {
         childImageSharp {
-          fixed(width: 300, height: 300) {
+          fixed(width: 250, height: 250) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      },
+      picker: file(relativePath: { eq: "product/picker.png" }) {
+        childImageSharp {
+          fixed(width: 250, height: 250) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      },
+      hackernews: file(relativePath: { eq: "product/hackernews.png" }) {
+        childImageSharp {
+          fixed(width: 250, height: 250) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      },
+      linebot: file(relativePath: { eq: "product/linebot.png" }) {
+        childImageSharp {
+          fixed(width: 250, height: 250) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      },
+      hugo: file(relativePath: { eq: "product/hugo.png" }) {
+        childImageSharp {
+          fixed(width: 250, height: 250) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      },
+      shapio: file(relativePath: { eq: "product/shapio.png" }) {
+        childImageSharp {
+          fixed(width: 250, height: 250) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      },
+      hmd: file(relativePath: { eq: "product/hmd.png" }) {
+        childImageSharp {
+          fixed(width: 250, height: 250) {
             ...GatsbyImageSharpFixed
           }
         }
@@ -34,36 +76,31 @@ export default () => {
   const products: Array<Product> = [
     {
       name: 'regist',
-      date: '2019/07',
-      description: 'GithubのGistビューア(iOS)',
-      techStack: [
-        'ReactNative', 'Expo', 'Github API', 'JavaScript', 'Sentry'
-      ],
-      badge:'<a href="https://apps.apple.com/us/app/regist/id1472456939?mt=8" style="display:inline-block;overflow:hidden;background:url(https://linkmaker.itunes.apple.com/ja-jp/badge-lrg.svg?releaseDate=2019-07-16&kind=iossoftware&bubble=ios_apps) no-repeat;width:135px;height:40px;"></a>',
-      webpage: 'https://apps.apple.com/us/app/regist/id1472456939',
-      image: data.regist.childImageSharp.fixed,
+      image: data.regist.childImageSharp.fixed
     },
     {
-      name: 'regist',
-      date: '2019/07',
-      description: 'GithubのGistビューア(iOS)',
-      techStack: [
-        'ReactNative', 'Expo', 'Github API', 'JavaScript', 'Sentry'
-      ],
-      badge:'<a href="https://apps.apple.com/us/app/regist/id1472456939?mt=8" style="display:inline-block;overflow:hidden;background:url(https://linkmaker.itunes.apple.com/ja-jp/badge-lrg.svg?releaseDate=2019-07-16&kind=iossoftware&bubble=ios_apps) no-repeat;width:135px;height:40px;"></a>',
-      webpage: 'https://apps.apple.com/us/app/regist/id1472456939',
-      image: data.regist.childImageSharp.fixed,
+      name: 'expo-multiple-media-imagepicker',
+      image: data.picker.childImageSharp.fixed
     },
     {
-      name: 'regist',
-      date: '2019/07',
-      description: 'GithubのGistビューア(iOS)',
-      techStack: [
-        'ReactNative', 'Expo', 'Github API', 'JavaScript', 'Sentry'
-      ],
-      badge:'<a href="https://apps.apple.com/us/app/regist/id1472456939?mt=8" style="display:inline-block;overflow:hidden;background:url(https://linkmaker.itunes.apple.com/ja-jp/badge-lrg.svg?releaseDate=2019-07-16&kind=iossoftware&bubble=ios_apps) no-repeat;width:135px;height:40px;"></a>',
-      webpage: 'https://apps.apple.com/us/app/regist/id1472456939',
-      image: data.regist.childImageSharp.fixed,
+      name: 'emacs-dashboard-hackernews',
+      image: data.hackernews.childImageSharp.fixed
+    },
+    {
+      name: 'この本いつ発売したっけ?',
+      image: data.linebot.childImageSharp.fixed
+    },
+    {
+      name: 'hugo-plesure-theme',
+      image: data.hugo.childImageSharp.fixed
+    },
+    {
+      name: 'SHAPIO',
+      image: data.shapio.childImageSharp.fixed
+    },
+    {
+      name: 'Enhancing Gaming Experience System Using See-Through HMD ',
+      image: data.hmd.childImageSharp.fixed
     }
   ]
 
@@ -71,35 +108,48 @@ export default () => {
     container: css({
       display: 'grid',
       gridGap: rhythm(2),
-      gridTemplateColumns: 'repeat(auto-fit, minmax(200px, 300px))',
+      gridTemplateColumns: 'repeat(auto-fit, minmax(250px, 250px))',
       justifyContent: 'center',
       margin: 'auto',
-      maxWidth: 1200,
+      maxWidth: 1200
     }),
     productContainer: css({
-      width: '300px',
-      height: '300px',
+      width: '250px',
+      height: '250px'
+    }),
+    productInner: css({
+      position: 'relative',
+      width: '100%',
+      height: '100%',
+      border: `0.2rem solid ${Colors.bubbles}`,
+      borderRadius: '125px',
+      overflow: 'hidden'
     }),
     name: css({
-      position: 'absolute'
+      position: 'absolute',
+      left: '50%',
+      top: '50%',
+      transform: 'translate(-50%, -50%)',
+      color: Colors.bubbles
     }),
     image: css({
-      objectFit: 'cover',
-      borderRadius: '150px',
-      background: Colors.bubbles,
-      boxShadow: '2px 2px 4px gray'
+      borderRadius: '125px',
+      filter: 'brightness(60%)',
+      margin: '-1px'
     })
   }
 
   return (
     <>
-      <SectionHeader title='Product'/>
+      <SectionHeader title='Product' />
       <div css={styles.container}>
         {products.map((p: Product) => {
           return (
             <div css={styles.productContainer}>
-              <h3 css={styles.name}>{p.name}</h3>
-              <Img css={styles.image} fixed={p.image} />
+              <div css={styles.productInner}>
+                <Img css={styles.image} fixed={p.image} />
+                <h3 css={styles.name}>{p.name}</h3>
+              </div>
             </div>
           )
         })}
