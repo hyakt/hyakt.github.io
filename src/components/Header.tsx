2f06d33088fabf33b4b4fe2619166675cb9caa98
hyakt <hyakt0@gmail.com>
2019-08-21 10:38:32 +0900

    update: wave

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -75,17 +75,19 @@ export default () => {
       </div>
       <svg xmlns="http://www.w3.org/2000/svg"
            css={styles.wave}
-           viewBox="0 0 54 14"
            preserveAspectRatio="none"
            aria-hidden="true"
-           focusable="false">
-        viewBox="0 0 54 14"
-        preserveAspectRatio="none"
-        aria-hidden="true"
-        focusable="false">
-        <path d="M 27 10.2878 C 20.3523 12.1439 13.3523 13.4963 0 13.4963 L 0 0 L 54 0 L 54 3.28785 C 40.6477 3.28785 33.6477 8.43177 27 10.2878 Z">
-          <animate attributeName="d" values="M 27 10C 21 8 14 3 0 3L 0 0L 54 0L 54 14C 40 14 33 12 27 10Z;M 27 14C 12 14 5 7 0 7L 0 0L 54 0L 54 7C 49 7 42 14 27 14Z;M 27 10C 21 12 14 14 0 14L 0 0L 54 0L 54 3C 40 3 33 8 27 10Z;M 27 10C 21 12 14 14 0 14L 0 0L 54 0L 54 3C 40 3 33 8 27 10Z;M 27 14C 12 14 5 7 0 7L 0 0L 54 0L 54 7C 49 7 42 14 27 14Z;M 27 10C 21 8 14 3 0 3L 0 0L 54 0L 54 14C 40 14 33 12 27 10Z" repeatCount="indefinite" dur="20s">
-          </animate>
+           focusable="false"
+           viewBox="0 0 100 30">
+        <path d="M100 0L100 7.5C85.5 11.85 76.44 14.57 72.82 15.65C66.18 17.65 59.04 17.09 52.79 14.09C47.99 11.78 54.73 15.02 52.79 14.09C38.34 7.15 21.11 9.88 9.51 20.94C8.24 22.14 5.07 25.17 0 30L0 0L100 0Z">
+          <animate attributeName="d" values="
+          M100 0L100 7C85 11 76 14 72 15C66 17 59 17 52 14C47 11 54 15 52 14C38 7 21 9 9 20C8 22 5 25 0 30L0 0L100 0Z;
+          M100 0L100 26C94 23 91 22 90 21C76 14 61 11 46 11C44 11 51 11 46 11C37 11 27 13 19 17C16 18 10 21 0 26L0 0L100 0Z;
+          M100 0L100 30C89 23 82 19 79 18C72 14 63 14 55 19C51 21 57 17 55 19C41 28 23 29 8 22C7 22 4 20 0 18L0 0L100 0Z;
+          M100 0L100 26C94 23 91 22 90 21C76 14 61 11 46 11C44 11 51 11 46 11C37 11 27 13 19 17C16 18 10 21 0 26L0 0L100 0Z;
+          M100 0L100 7C85 11 76 14 72 15C66 17 59 17 52 14C47 11 54 15 52 14C38 7 21 9 9 20C8 22 5 25 0 30L0 0L100 0Z
+          "
+          repeatCount="indefinite" dur="20s" />
         </path>
       </svg>
     </header>
