7559050869342171ed2e293089d11ec07d23feae
hyakt <hyakt0@gmail.com>
2019-08-26 22:53:14 +0900

    fix: path

--- src/pages/privacy.tsx
+++ src/pages/privacy.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 import Layout from '../components/Layout'
 import Header from '../components/Header'
-import Privacy from '../content/Privacy'
+import Privacy from '../contents/Privacy'
 import Footer from '../components/Footer'
 import SEO from '../components/SEO'
 
