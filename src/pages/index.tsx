5fa9f32c2bccba4150d29a41cb861e98f51a5363
hyakt <hyakt0@gmail.com>
2020-02-11 13:21:11 +0900

    remove : about

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 
 import Layout from '../components/Layout'
 import Header from '../components/Header'
-import About from '../sections/About'
 import Skills from '../sections/Skills'
 import Products from '../sections/Products'
 import GithubStreak from '../sections/GithubStreak'
@@ -12,7 +11,6 @@ import SEO from '../components/SEO'
 const IndexPage = () => (
   <Layout>
     <Header />
-    <About />
     <Skills />
     <GithubStreak />
     <Products />
