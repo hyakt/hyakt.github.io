41984cdc742fc43bcc6235ca0ed5256fe198757d
hyakt <hyakt0@gmail.com>
2021-07-15 00:03:58 +0900

    remove: GithubStreak

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,19 @@
-import React from 'react'
+import React from "react"
 
-import { Layout } from '../components/Layout'
-import { Header } from '../components/Header'
-import { Skills } from '../sections/Skills'
-import { Products } from '../sections/Products'
-import { GithubStreak } from '../sections/GithubStreak'
-import { Footer } from '../components/Footer'
-import { SEO } from '../components/SEO'
+import { Layout } from "../components/Layout"
+import { Header } from "../components/Header"
+import { Skills } from "../sections/Skills"
+import { Products } from "../sections/Products"
+import { GithubStreak } from "../sections/GithubStreak"
+import { Footer } from "../components/Footer"
+import { SEO } from "../components/SEO"
 
 const IndexPage: React.FC = () => (
   <Layout>
     <Header />
     <Skills />
-    <GithubStreak />
     <Products />
-    <SEO title='Portfolio' />
+    <SEO title="Portfolio" />
     <Footer />
   </Layout>
 )
