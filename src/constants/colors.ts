035d0ca89823036697f60092f4397c806ae5850f
hyakt <hyakt0@gmail.com>
2021-10-07 09:28:36 +0900

    add: colors

---
+++ src/constants/colors.ts
@@ -0,0 +1,11 @@
+const colors = {
+  fg: '#2a2a2a',
+  bubbles: '#F2F2F2',
+  pilsner: '#F2A007',
+  paleale: '#F2B705',
+  amber: '#BF0413',
+  stout: '#1F1705',
+  brewdog: '#43afdd'
+}
+
+export default colors
