0288de6a46252a3589d1d568a67f1c27ba3a77aa
hyakt <hyakt0@gmail.com>
2021-10-09 00:27:23 +0900

    Revert "remove: unused font"
    This reverts commit de735f53e08e26ea5e622d18ba7dabc8e737ac9e.

--- src/utils/typography.ts
+++ src/utils/typography.ts
@@ -3,8 +3,13 @@ import Typography from 'typography'
 const typography = new Typography({
   baseFontSize: '18px',
   baseLineHeight: 1.666,
+  headerFontFamily: ['Montserrat', 'sans-serif'],
   bodyFontFamily: ['Noto Sans JP', 'Georgia', 'serif'],
   googleFonts: [
+    {
+      name: 'Montserrat',
+      styles: ['300', '400', '500', '700', '900']
+    },
     {
       name: 'Noto Sans JP',
       styles: ['300', '400', '500', '700', '900']
